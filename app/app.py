62fb0359f12cf8f16a6a9c8bb7d3b5cf510e94aa
Rudra-QA <rudra.chunduri@gmail.com>
2026-08-31 11:32:57 +0000

    Containerize Flask application

--- app/app.py
+++ app/app.py
@@ -1,6 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
 def hello():
-    return "Hello from DevSecOps practice!"
+    return "Hello from DevOps Practice!"
 
 
 if __name__ == "__main__":
-    print(hello())
+    app.run(host="0.0.0.0", port=5000)
